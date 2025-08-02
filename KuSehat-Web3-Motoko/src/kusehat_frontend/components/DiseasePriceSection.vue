c05ac350133c8e7786030f85cfee75e52860f9aa
Sir Ananda Rauf <sirrauf412@gmail.com>
2025-08-02 13:11:38 +0700

    File script Flask,MotokoAPI, Nuxt3, dan file model dataset penyakit

---
+++ KuSehat-Web3-Motoko/src/kusehat_frontend/components/DiseasePriceSection.vue
@@ -0,0 +1,26 @@
+<template>
+  <section id="diseaseprice">
+    <h2>List Disease Exchange Price</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>Jenis Data</th>
+          <th>Harga Tukar (IDR)</th>
+          <th>Tujuan</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Gambar Penyakit Kulit Parah seperti Kanker Kulit dan Gambar Penyakit Kelamin Parah Seperti Raja Singa dan Kanker Payudara</td>
+          <td>IDR. 100.000</td>
+          <td>Data medis Rumah Sakit</td>
+        </tr>
+        <tr>
+          <td>Gambar Penyakit Kelamin Parah Seperti Raja Singa dan Kanker Payudara</td>
+          <td>IDR. 200.000</td>
+          <td>Data medis penyakit dan Pelatihan Model Diagnosis AI</td>
+        </tr>
+      </tbody>
+    </table>
+  </section>
+</template>
