ed8b97112c3012f55eb814d40ea6a60fa8fd67c3
Sir Ananda Rauf <sirrauf412@gmail.com>
2025-08-17 11:49:31 +0700

    Update kusehat.sql

--- Database/kusehat.sql
+++ Database/kusehat.sql
@@ -1,75 +1,143 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Versi server:                 8.0.30 - MySQL Community Server - GPL
--- OS Server:                    Win64
--- HeidiSQL Versi:               12.1.0.6537
--- --------------------------------------------------------
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 17, 2025 at 06:48 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `kusehat`
+--
 
--- Membuang struktur basisdata untuk kusehat
-DROP DATABASE IF EXISTS `kusehat`;
-CREATE DATABASE IF NOT EXISTS `kusehat` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `kusehat`;
+-- --------------------------------------------------------
 
--- membuang struktur untuk table kusehat.exchange
-DROP TABLE IF EXISTS `exchange`;
-CREATE TABLE IF NOT EXISTS `exchange` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user` int NOT NULL,
+--
+-- Table structure for table `exchange`
+--
+
+CREATE TABLE `exchange` (
+  `id` int(11) NOT NULL,
+  `user` int(11) NOT NULL,
   `tujuan` varchar(255) NOT NULL,
   `gambar` varchar(255) NOT NULL,
   `diagnosa` varchar(255) NOT NULL,
   `tanggal` datetime NOT NULL,
-  `saldoreward` double NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idx_exchange__user` (`user`),
-  CONSTRAINT `fk_exchange__user` FOREIGN KEY (`user`) REFERENCES `user` (`userid`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `saldoreward` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Pengeluaran data tidak dipilih.
+-- --------------------------------------------------------
 
--- membuang struktur untuk table kusehat.topup
-DROP TABLE IF EXISTS `topup`;
-CREATE TABLE IF NOT EXISTS `topup` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `user` int NOT NULL,
+--
+-- Table structure for table `topup`
+--
+
+CREATE TABLE `topup` (
+  `id` int(11) NOT NULL,
+  `user` int(11) NOT NULL,
   `jumlah` double NOT NULL,
   `metode` varchar(255) NOT NULL,
-  `tanggal` datetime NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `idx_topup__user` (`user`),
-  CONSTRAINT `fk_topup__user` FOREIGN KEY (`user`) REFERENCES `user` (`userid`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `tanggal` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Pengeluaran data tidak dipilih.
+-- --------------------------------------------------------
 
--- membuang struktur untuk table kusehat.user
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE IF NOT EXISTS `user` (
-  `userid` int NOT NULL AUTO_INCREMENT,
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `userid` int(11) NOT NULL,
   `namauser` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `register_date` datetime NOT NULL,
   `login_date` datetime DEFAULT NULL,
-  `saldo` double NOT NULL,
-  PRIMARY KEY (`userid`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `saldo` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Pengeluaran data tidak dipilih.
+--
+-- Dumping data for table `user`
+--
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+INSERT INTO `user` (`userid`, `namauser`, `email`, `password`, `register_date`, `login_date`, `saldo`) VALUES
+(1, 'Sir Rauf', 'anandaraufm@gmail.com', 'test123', '2025-08-17 11:48:09', NULL, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `exchange`
+--
+ALTER TABLE `exchange`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_exchange__user` (`user`);
+
+--
+-- Indexes for table `topup`
+--
+ALTER TABLE `topup`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_topup__user` (`user`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userid`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `exchange`
+--
+ALTER TABLE `exchange`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `topup`
+--
+ALTER TABLE `topup`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `exchange`
+--
+ALTER TABLE `exchange`
+  ADD CONSTRAINT `fk_exchange__user` FOREIGN KEY (`user`) REFERENCES `user` (`userid`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `topup`
+--
+ALTER TABLE `topup`
+  ADD CONSTRAINT `fk_topup__user` FOREIGN KEY (`user`) REFERENCES `user` (`userid`) ON DELETE CASCADE;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
