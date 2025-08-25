4045ed4548761a2995b1344616157b571056ec80
Sir Ananda Rauf <sirrauf412@gmail.com>
2025-08-25 23:19:50 +0700

    Delete KuSehat-Web3-Motoko directory

--- KuSehat-Web3-Motoko/kusehat_frontend/nuxt.config.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtConfig({
-  ssr: false, // Penting untuk aplikasi IC agar berjalan di client-side
-  vite: {
-    optimizeDeps: {
-      include: ["@dfinity/agent", "@dfinity/candid"],
-    },
-  },
-});
